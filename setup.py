db7a1cc292acf077d91fe8fbac7dec370c2b3b91
Patrick Crownover <pcrownov@redhat.com>
2015-10-08 14:58:56 -0400

    updating versioning

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
 requires = ['pyramid', 'repoze.who', 'unittest2']
 
 setup(name='pyramid_whoauth',
-      version='0.1.2',
+      version='0.1.2-rh',
       description='pyramid_whoauth',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
